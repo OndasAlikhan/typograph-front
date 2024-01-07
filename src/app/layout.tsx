267b6986fa62a166b1f075a37ec77a81bff8b917
Alikhan Ondas <alihan.ondas@gmail.com>
2024-01-07 17:17:57 +0600

    feat: add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Footer } from "@/components/common/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppinsFont = Poppins({
     weight: ["300", "400", "500", "600", "700"],
@@ -32,6 +33,7 @@ export default function RootLayout({
                 <Footer />
             </body>
             <Analytics />
+            <SpeedInsights />
         </html>
     );
 }
